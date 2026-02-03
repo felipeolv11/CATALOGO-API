18cb08612a1d3fa397f112acc446ac0fc94f6b75
felipeolv11 <felipeeh1524@gmail.com>
2026-02-03 00:04:03 -0300

    ajustar repositório genérico para uso do unit of work

--- Catalogo API/Repositories/Repository.cs
+++ Catalogo API/Repositories/Repository.cs
@@ -1,5 +1,6 @@
 ﻿using Catalogo_API.Context;
 using Catalogo_API.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
 using System.Linq.Expressions;
 
 namespace Catalogo_API.Repositories;
@@ -15,7 +16,7 @@ public class Repository<T> : IRepository<T> where T : class
 
     public IEnumerable<T> GetAll()
     {
-        return _context.Set<T>().ToList();
+        return _context.Set<T>().AsNoTracking().ToList();
     }
 
     public T? Get(Expression<Func<T, bool>> predicate)
@@ -26,21 +27,18 @@ public class Repository<T> : IRepository<T> where T : class
     public T Create(T entity)
     {
         _context.Set<T>().Add(entity);
-        _context.SaveChanges();
         return entity;
     }
 
     public T Update(T entity)
     {
         _context.Set<T>().Update(entity);
-        _context.SaveChanges();
         return entity;
     }
 
     public T Delete(T entity)
     {
         _context.Set<T>().Remove(entity);
-        _context.SaveChanges();
         return entity;
     }
 }
